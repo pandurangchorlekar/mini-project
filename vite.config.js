5cd43bb2a27ec1e48bf5f73ac09a217b518ba358
pandurangchorlekar <pandurangchorlekar94@gmai.com>
2025-11-21 23:44:25 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/mini-project/',   // MUST match your GitHub repo name
+  base: './',   // MUST match your GitHub repo name
   plugins: [react()],
 })
